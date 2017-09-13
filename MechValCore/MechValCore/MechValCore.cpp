a137ad72d14e5541f52994d525e326869f36c382
Christophe Delage <christ@ophe.net>
2017-09-13 20:14:38 +0200

    dummy call to nlopt in MechValCore, and it works!

--- MechValCore/MechValCore/MechValCore.cpp
+++ MechValCore/MechValCore/MechValCore.cpp
@@ -2,9 +2,17 @@
 //
 
 #include "MechValCore.h"
+#include "nlopt.h"
 #include "pouet.h"
 
 extern "C" {
 // This is an example of an exported function.
-MechValCore_API int fnMechValCore (void) { return pouet (); }
+MechValCore_API int fnMechValCore (void)
+{
+    nlopt_opt opt = nlopt_create (NLOPT_LN_COBYLA, 3);
+    int dim       = nlopt_get_dimension (opt);
+    nlopt_destroy (opt);
+
+    return pouet () + dim;
+}
 }
